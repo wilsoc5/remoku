38e687a846af420a711eca0866c721eae6dac5c5
gonzotek@gmail.com <gonzotek@gmail.com@ab0ec3bd-c85b-b755-42fc-a4ff9edab1fc>
2012-02-12 23:36:44 +0000

    Firefox extension

---
+++ firefox-addon/chrome/button.js
@@ -0,0 +1,40 @@
+Components.utils.import("resource://gre/modules/Services.jsm");
+if (typeof remoku == "undefined") {  
+  var remoku = {  
+    init : function() {  
+	    var firstrun = Services.prefs.getBoolPref("extensions.remoku.installed");
+		var curVersion = "0.0.0";
+		if (!firstrun) {
+		  Services.prefs.setBoolPref("extensions.remoku.installed", true);
+		    remoku.installButton("nav-bar", "remoku-button-1");
+		    remoku.installButton("addon-bar", "remoku-button-1");
+		} else {
+		  try {
+		    var installedVersion = Services.prefs.getCharPref("extensions.remoku.installedVersion");
+		    if (curVersion > installedVersion) {
+		      Services.prefs.setCharPref("extensions.remoku.installedVersion", curVersion);
+		    }
+		  } catch (ex) {
+		  }
+		}
+	    },
+	installButton : function(toolbarId, id, afterId){
+		if (!document.getElementById(id)) {
+        var toolbar = document.getElementById(toolbarId);
+        var before = null;
+        if (afterId) {
+            let elem = document.getElementById(afterId);
+            if (elem && elem.parentNode == toolbar)
+                before = elem.nextElementSibling;
+        }
+        toolbar.insertItem(id, before);
+        toolbar.setAttribute("currentset", toolbar.currentSet);
+        document.persist(toolbar.id, "currentset");
+        if (toolbarId == "addon-bar")
+            toolbar.collapsed = false;
+    	}	
+	}
+  };  
+};
+
+window.addEventListener("load", remoku.init, false);
---
+++ firefox-addon/defaults/preferences/remoku-prefs.js
@@ -0,0 +1,1 @@
+pref("extensions.remoku.installed", false);
